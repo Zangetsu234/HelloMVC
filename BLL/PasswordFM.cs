c63a4f3bd53bbf0ca0aeeee1672c96598abf73ee
Colton <coltonpersonal@gmail.com>
2013-12-10 17:07:13 -0500

    Setup user change password

---
+++ BLL/PasswordFM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class PasswordFM
+    {
+        public int ID { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string VerifyPassword { get; set; }
+        public PasswordFM(User user)
+        {
+            this.CurrentPassword = user.Password;
+            this.NewPassword = user.Password;
+            this.VerifyPassword = user.Password;
+            this.ID = user.ID;
+        }
+        public PasswordFM()
+	    {
+
+	    }
+
+    }
+}
